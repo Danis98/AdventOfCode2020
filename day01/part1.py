96008fb28e6e078ac8afa58fcdce859b03c61e74
Daniele Venier <danielevenier1998@gmail.com>
2020-12-01 05:39:28 +0000

    Add helpers

--- day01/part1.py
+++ day01/part1.py
@@ -1,4 +1,9 @@
-input = list(map(int, open("day01.input", "r").read().strip().split("\n")))
+def lmap(func, x):
+    return list(map(func, x))
+
+# --------- END OF HELPER FUNCS ---------
+
+input = lmap(int, open("day01.input", "r").read().strip().split("\n"))
 
 for num in input:
     if 2020-num in input:
--- day01/part2.py
+++ day01/part2.py
@@ -1,11 +1,19 @@
-input = list(map(int, open("day01.input", "r").read().strip().split("\n")))
+import itertools
 
-f=False
-for a in input:
-    for b in input:
-        if 2020-a-b in input:
-            print(a * b * (2020-a-b))
-            f=True
-            break
-    if f:
+def lmap(func, x):
+    return list(map(func, x))
+
+def pairs(x):
+    return itertools.combinations(x, 2)
+
+def pairs(x, y):
+    return itertools.product(x, y)
+
+# --------- END OF HELPER FUNCS ---------
+
+input = lmap(int, open("day01.input", "r").read().strip().split("\n"))
+
+for a, b in pairs(input):
+    if 2020-a-b in input:
+        print(a * b * (2020-a-b))
         break
