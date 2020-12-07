bc82296441f797d8747697d022b0653024473c57
Daniele Venier <danielevenier1998@gmail.com>
2020-12-07 05:25:20 +0000

    Remove helper spam

--- day06/part2.py
+++ day06/part2.py
@@ -1,21 +1,3 @@
-import itertools
-from functools import reduce
-import operator
-
-def lmap(func, x):
-    return list(map(func, x))
-
-def pairs(x):
-    return itertools.combinations(x, 2)
-
-def pairs(x, y):
-    return itertools.product(x, y)
-
-def mult(a):
-    return reduce(operator.mul, a, 1)
-
-# --------- END OF HELPER FUNCS ---------
-
 input = open("day06.input", "r").read().strip().split("\n")
 
 res = 0
